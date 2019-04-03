36ed52b8edd5ddb29ed1c51539a69891489d4f10
Helene Tindon <helene.tindon@gmail.com>
2019-04-03 19:22:47 +0200

    moving resources to new resources' folder and updating constants accordingly

--- Pygame_DK/constants.py
+++ Pygame_DK/constants.py
@@ -1,8 +1,8 @@
 window_size = 450
 
 # Images
-icon_img = "depart.png"
-menu_bg_img = "accueil.png"
+icon_img = "resources/images/depart.png"
+menu_bg_img = "resources/images/accueil.png"
 
 # Textual values
 window_title = "DK Labyrinth"
